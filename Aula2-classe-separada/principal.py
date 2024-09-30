95457a698c9b7039b12faa2d0eba366fb3e8e40b
Mércia <serpamercia@gamil.com>
2024-09-30 15:48:55 -0300

    Alterando arquivos do git hub

---
+++ Aula2-classe-separada/principal.py
@@ -0,0 +1,17 @@
+#Estamos acessando o arquivo 'pessoa' e dentor desse arquivo estamos importando a classe 'Pessoa'
+from pessoa import Pessoa
+
+#Criando Objetos
+
+p1 = Pessoa("Joana","Correr","Rua dos alfinetes 2121")
+p1.exibirHobby()
+p1.mudarHobby("Natação")
+
+#SOLICITANDO DADOS DO USUÁRIO
+nomePessoa = input("Informe o nome da pessoa: ")
+hobbyPessoa = input("Informe o hobby da pessoa: ")
+endPessoa = input("Qual o endereço? ")
+
+p2 = Pessoa(nomePessoa, hobbyPessoa, endPessoa)
+p2.exibirHobby()
+p2.mudarHobby("Dançar")
