0a3387715d7dfb3e94da47afc6c538dc1ee077b8
Mércia <serpamercia@gmail.com>
2024-10-14 14:54:24 -0300

    Banco de dados

---
+++ banco de dados/funcionario_poo.py
@@ -0,0 +1,50 @@
+import sqlite3
+class Funcionario:
+    def conexao(self):
+        conexao = sqlite3.connect("departamento.db")
+        consulta = conexao.cursor()
+        tabela = """
+        CREATE TABLE IF NOT EXISTS funcionario(
+        codigo INTEGER PRIMARY KEY AUTOINCREMENT,
+        nome VARCHAR(100),
+        salario FLOAT,
+        cargo VARCHAR(100)    
+        );
+        """
+        
+        consulta.execute(tabela)
+        return conexao
+    
+    def inserir(self, nome, salario, cargo):
+        conexao = self.conexao()#estamos chamando o métodp que irá conectar ao banco, esse método foi criado mais acima
+        
+        sql = "INSERT INTO funcionario VALUES (?,?,?,?)"
+        
+        campos = (None, nome, salario, cargo)
+        
+        consulta = conexao.cursor()
+        consulta.execute(sql, campos)
+        
+        conexao.commit()
+        
+        print(consulta.rowcount, "Linha(s) inserida(s) com sucesso")
+        
+        conexao.close()
+    
+    def consultar(self):
+        conexao = self.conexao()
+        consulta = conexao.cursor()
+        
+        sql = "SELECT * FROM funcionario"
+        consulta.execute(sql)
+        
+        resultado = consulta.fetchall()
+        
+        for itens in resultado:
+            print(f"Código: {itens[0]}")
+            print(f"Nome: {itens[1]}")
+            print(f"Salário: {itens[2]}")
+            print(f"Cargo: {itens[3]}")
+            print("-"*40)#Criando um separador entre os registro
+            
+        conexao.close()
